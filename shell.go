63d3dc574a94f8b819c13c714e87fdb6bfc09d25
Sebastian Stark <stark@tuebingen.mpg.de>
2019-04-02 09:21:22 +0200

    handle error in shell

--- shell.go
+++ shell.go
@@ -8,10 +8,17 @@ import (
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 	knx := NewClient("")
-	_, si := knx.GetServerItem()
+
+	err, si := knx.GetServerItem()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("%s fw:%d sn:%v\n", knx.Url, si.FirmwareVersion, si.SerialNumber)
 
-	_, dpd := knx.GetDatapointDescription([]int{700, 701, 711, 712, 720, 721, 722})
+	err, dpd := knx.GetDatapointDescription([]int{700, 701, 711, 712, 720, 721, 722, 234324})
+	if err != nil {
+		log.Fatal(err)
+	}
 	for _, d := range dpd {
 		fmt.Printf("%d:%d\n", d.Datapoint, d.DatapointType)
 	}
